a9e1507c247ec80585aaa8ed9b91bf7457e7671c
HalimaMTH <moutchouhalima19@gmail.com>
2026-04-19 23:14:08 +0100

    des modification de test

--- projet_data_mining/backend/app/services/preprocessing.py
+++ projet_data_mining/backend/app/services/preprocessing.py
@@ -7,7 +7,7 @@ def preprocess_pipeline(path):
     df['price'] = pd.to_numeric(df['price'], errors='coerce')
     df = df.dropna(subset=['price'])
 
-    df['currency'] = df['currency'].fillna('MAD')
+    df['currency'] = df['currency'].fillna('MAD') 
 
     rates = {'MAD': 1, 'USD': 10, 'EUR': 11}
     df['price_mad'] = df.apply(lambda x: x['price'] * rates[x['currency']], axis=1)
